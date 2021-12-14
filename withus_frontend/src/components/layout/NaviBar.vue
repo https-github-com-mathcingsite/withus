c5a5dd558c3f6d52da30b6c3ec44dc0eac7a90df
KwonOhBeom <xpzmfhf@gmail.com>
2021-12-15 04:29:09 +0900

    Build: 메인 페이지 구조

---
+++ withus_frontend/src/components/layout/NaviBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar class="" app color="white">
+    <v-img width="120" src="@/assets/Logo.png"></v-img>
+    <v-app-bar-title class="font-weight-bold" style="overflow: visible">프로젝트 이름</v-app-bar-title>
+    <v-tabs class="ml-2" color="purple lighten-3">
+      <v-tab>팀원 찾기</v-tab>
+      <v-tab>팀원 모집</v-tab>
+      <v-tab>알림</v-tab>
+    </v-tabs>
+    <v-spacer></v-spacer>
+    <v-badge bordered dot left color="error">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="grey lighten-1" rounded dark v-bind="attrs" v-on="on">
+            <v-icon>mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(menu, index) in menues" :key="index">
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-badge>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menues: [{ title: "내 작성글" }, { title: "내 관심글" }, { title: "내 쪽지함" }, { title: "내 정보 수정" }],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ withus_frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <navi-bar />
+    <slide-bar />
+    <search-bar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import NaviBar from "@/components/layout/NaviBar.vue";
+import SearchBar from "@/components/layout/SearchBar.vue";
+import SlideBar from "@/components/layout/SlideBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NaviBar,
+    SearchBar,
+    SlideBar,
+  },
+};
+</script>

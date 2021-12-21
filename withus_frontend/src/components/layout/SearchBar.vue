a47203924bb66df8cd9249cfabe3a56b0a798b61
leen6225 <leen6225@gmail.com>
2021-12-22 00:58:52 +0900

    <style>(layout) : prettier 세팅 적용 -> package.json 에 prettier rule을 적용하여 del warnings 제거

--- withus_frontend/src/components/layout/SearchBar.vue
+++ withus_frontend/src/components/layout/SearchBar.vue
@@ -3,15 +3,27 @@
     <v-row app>
       <v-col cols="9" align-self="center">
         <v-item-group multiple>
-          <v-item v-for="(n, idx) in options" :key="idx" v-slot="{ active, toggle }">
-            <v-chip class="ml-2" active-class="purple--text" :input-value="active" @click="toggle">{{ n.name }} </v-chip>
+          <v-item
+            v-for="(n, idx) in options"
+            :key="idx"
+            v-slot="{ active, toggle }"
+          >
+            <v-chip
+              class="ml-2"
+              active-class="purple--text"
+              :input-value="active"
+              @click="toggle"
+              >{{ n.name }}
+            </v-chip>
           </v-item>
         </v-item-group>
       </v-col>
       <v-col cols="2" align-self="center" align="left">
         <v-text-field placeholder="검색"></v-text-field>
       </v-col>
-      <v-col align-self="center"> <v-btn tile color="purple lighten-3" dark> Search </v-btn> </v-col>
+      <v-col align-self="center">
+        <v-btn tile color="purple lighten-3" dark> Search </v-btn>
+      </v-col>
     </v-row>
   </v-card>
 </template>
@@ -20,7 +32,16 @@
 export default {
   data() {
     return {
-      options: [{ name: "JAVA" }, { name: "C/C++" }, { name: "Python" }, { name: "Spring" }, { name: "Vue.js" }, { name: "React.js" }, { name: "Swift" }, { name: "Android" }],
+      options: [
+        { name: "JAVA" },
+        { name: "C/C++" },
+        { name: "Python" },
+        { name: "Spring" },
+        { name: "Vue.js" },
+        { name: "React.js" },
+        { name: "Swift" },
+        { name: "Android" },
+      ],
     };
   },
 };

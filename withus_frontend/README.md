9d8d16c6ecc657635832dd4a87f4278f80856334
leen6225 <leen6225@gmail.com>
2021-12-22 01:34:03 +0900

    <style> (all) : package.json 에 install 된 파일 설정 및 공백 제거

--- withus_frontend/README.md
+++ withus_frontend/README.md
@@ -1,24 +1,35 @@
 # withus_frontend
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
+### Install Material Icons
+
+```
+npm install material-design-icons-iconfont -D
+```
+
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- withus_frontend/src/components/layout/SlideBar.vue
+++ withus_frontend/src/components/layout/SlideBar.vue
@@ -1,7 +1,12 @@
 <template>
   <v-card app>
     <v-carousel cycle height="300" hide-delimiters show-arrows-on-hover>
-      <v-carousel-item v-for="(picture, i) in pictures" :key="i" :src="require(`@/assets/${picture.name}`)"> </v-carousel-item>
+      <v-carousel-item
+        v-for="(picture, i) in pictures"
+        :key="i"
+        :src="require(`@/assets/${picture.name}`)"
+      >
+      </v-carousel-item>
     </v-carousel>
   </v-card>
 </template>
@@ -10,7 +15,11 @@
 export default {
   data() {
     return {
-      pictures: [{ name: "Slide_1.jpg" }, { name: "Slide_2.jpg" }, { name: "Slide_3.jpg" }],
+      pictures: [
+        { name: "Slide_1.jpg" },
+        { name: "Slide_2.jpg" },
+        { name: "Slide_3.jpg" },
+      ],
     };
   },
 };

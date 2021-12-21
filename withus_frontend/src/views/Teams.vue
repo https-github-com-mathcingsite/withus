43541f76e5774ea2e2ccf7e3fa3ac9efff801197
leen6225 <leen6225@gmail.com>
2021-12-22 01:03:57 +0900

    <feat> (teams) : 팀 모집 관련 UI

---
+++ withus_frontend/src/views/Teams.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <navi-bar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import NaviBar from "@/components/layout/NaviBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NaviBar,
+  },
+};
+</script>
